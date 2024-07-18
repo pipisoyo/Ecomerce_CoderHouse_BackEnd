7e732ed840e503ec67c637abbfb4f34766382574
Ángel Nicolás Di Falco <difalcoangelnicolas@gmail.com>
2024-07-17 23:51:57 -0300

    Refactor user controllers to use object-oriented approach

--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -1,42 +1,37 @@
 import response from '../config/responses.js';
-import userModel from '../dao/models/users.js'
+import userModel from '../dao/models/users.js';
 import UserManager from '../dao/mongo/user.mongo.js';
 import { addLogger } from '../utils/logger.js';
 import { __dirname } from '../config.js';
 import fs from 'fs';
 
-const users = new UserManager()
+const users = new UserManager();
 
-const userContorler = {
-
-
-    getAll: async (req,res) => {
-        addLogger (req,res, async ()=>{
-            req.logger.info('obteniendo usuarios')
+const userController = {
+    getAll: async function(req, res) {
+        addLogger(req, res, async function() {
+            req.logger.info('obteniendo usuarios');
             try {
                 const result = await users.getAll();
                 const usersResponse = result.map(user => {
                     return {
                         first_name: user.first_name,
-                        email : user.email,
-                        role : user.role
-                    }
-                })
-                req.logger.info('Usuarios recuperados')
-                return response.successResponse(res,200,"Usuarios recuperado con exito",usersResponse)
+                        email: user.email,
+                        role: user.role
+                    };
+                });
+                req.logger.info('Usuarios recuperados');
+                return response.successResponse(res, 200, "Usuarios recuperados con éxito", usersResponse);
             } catch (error) {
-                req.logger.error("Error al recuperar los usuarios")
-                return response.errorResponse(res,500,"Error al recuperar los usuarios")
+                req.logger.error("Error al recuperar los usuarios");
+                return response.errorResponse(res, 500, "Error al recuperar los usuarios");
             }
-        })
+        });
     },
 
-    premiun: (req, res) => {
-        /**
-        * @param {string} req.params.uid - ID del usuario a actualizar.
-        */
-        addLogger(req, res, async () => {
-            req.logger.info('Verificando informacion');
+    premiun: function(req, res) {
+        addLogger(req, res, async function() {
+            req.logger.info('Verificando información');
             const uid = req.params.uid;
             userModel.findById(uid).then(user => {
                 if (!user) {
@@ -44,8 +39,8 @@ const userContorler = {
                     return response.errorResponse(res, 404, 'No se encuentra el usuario');
                 }
 
-                if (user.role === "user"){
-                    req.logger.debug('Verificando documentacion requerida');
+                if (user.role === "user") {
+                    req.logger.debug('Verificando documentación requerida');
                     const requiredDocuments = ['identificacion', 'domicilio', 'cuenta'];
                     const userDocuments = user.documents.map(document => document.name);
                     if (requiredDocuments.some(doc => !userDocuments.includes(doc))) {
@@ -53,9 +48,9 @@ const userContorler = {
                         return response.errorResponse(res, 400, 'Faltan documentos requeridos en la base de datos para actualizar a premium');
                     }
                 }
-    
+
                 let newRole = user.role === "premiun" ? "user" : "premiun";
-    
+
                 userModel.updateOne({ _id: uid }, { role: newRole }).then(() => {
                     req.logger.info(`Rol actualizado a ${newRole}`);
                     response.successResponse(res, 200, `Rol actualizado a ${newRole}, por favor vuelva a iniciar sesión`, null);
@@ -68,43 +63,41 @@ const userContorler = {
         });
     },
 
-    uploadDocuments: async (req, res) => {
+    uploadDocuments: async function(req, res) {
         const { uid } = req.params;
         const files = req.files;
-        addLogger(req,res,async ()=>{
+        addLogger(req, res, async function() {
             try {
                 const user = await userModel.findById(uid);
                 if (!user) {
-                    req.logger.error("Usuario no encontrado")
+                    req.logger.error("Usuario no encontrado");
                     return response.errorResponse(res, 404, 'Usuario no encontrado');
                 }
                 Object.keys(files).forEach(field => {
                     const file = files[field][0];
-                    const fieldname=file.filename
+                    const fieldname = file.filename;
                     const existingDocumentIndex = user.documents.findIndex(doc => doc.name === field);
                     if (existingDocumentIndex !== -1) {
                         const oldFilePath = user.documents[existingDocumentIndex].reference;
-                        fs.unlinkSync(oldFilePath)// Eliminar el archivo anterior
+                        fs.unlinkSync(oldFilePath); // Eliminar el archivo anterior
                         user.documents[existingDocumentIndex].reference = file.path;
-                        req.logger.info(`Documento ${field} actualizado correctamente`) // Sobrescribir el documento existente
+                        req.logger.info(`Documento ${field} actualizado correctamente`); // Sobrescribir el documento existente
                     } else {
-                        user.documents.push({ name: field, reference: file.path }); 
-                        req.logger.info("Documento cargado correctamente")// Agregar nuevo documento
+                        user.documents.push({ name: field, reference: file.path });
+                        req.logger.info("Documento cargado correctamente"); // Agregar nuevo documento
                     }
                 });
                 
                 // Actualizar el estado del usuario
                 user.status = 'Documentos subidos';
-                console.log("🚀 ~ addLogger ~ user:", user)
                 await user.save();
                 return response.successResponse(res, 200, 'Documentos subidos exitosamente');
             } catch (error) {
-                req.logger.error("Error al subir los documentos")
+                req.logger.error("Error al subir los documentos");
                 return response.errorResponse(res, 500, 'Error al subir los documentos');
-            };
+            }
         });
     }
 };
 
-
-export default userContorler;
+export default userController;
